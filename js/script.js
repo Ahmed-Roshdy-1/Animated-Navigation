f243295dd1b51d70afc0f1522e8d39d058211160
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-08-14 11:42:37 +0200

    push

--- js/script.js
+++ js/script.js
@@ -10,18 +10,27 @@ function toggleNav() {
   // toggle: Menu Bars Open/Closed
   menuBars.classList.toggle("change");
   // toggle: Menu Bars Active
-  overlay.classList.toggle("overlay-active");
-  if (overlay.classList.contains("overlay-active")) {
+
+  if (overlay.classList.contains("overlay-slide-left")) {
     // Animate In - Overlay
-    overlay.classList.remove("overlay-slide-left");
-    overlay.classList.add("overlay-slide-right");
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    // Animate In - Nav Items
+    nav1.classList.replace("slide-out-1", "slide-in-1");
+    nav2.classList.replace("slide-out-2", "slide-in-2");
+    nav3.classList.replace("slide-out-3", "slide-in-3");
+    nav4.classList.replace("slide-out-4", "slide-in-4");
+    nav5.classList.replace("slide-out-5", "slide-in-5");
   } else {
-    // Animate out - Overlay
-    overlay.classList.remove("overlay-slide-right");
-    overlay.classList.add("overlay-slide-left");
+    // 5nimate out - Overlay
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    // 5nimate out - Nav Items
+    nav1.classList.replace("slide-in-1", "slide-out-1");
+    nav2.classList.replace("slide-in-2", "slide-out-2");
+    nav3.classList.replace("slide-in-3", "slide-out-3");
+    nav4.classList.replace("slide-in-4", "slide-out-4");
+    nav5.classList.replace("slide-in-5", "slide-out-5");
   }
 }
-
 // Event Listeners
 menuBars.addEventListener("click", toggleNav);
 nav1.addEventListener("click", toggleNav);
